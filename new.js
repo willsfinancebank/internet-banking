91d14734df35110d387f9ae986393ca197bad33a
willsfinancebank <helenprivate2013@gmail.com>
2026-08-01 05:57:43 +0100

    Create new.js

---
+++ new.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB-ejXt-72-0KJd5cvptzOtdEM10F9a-Z4",
+  authDomain: "data-3715b.firebaseapp.com",
+  databaseURL: "https://data-3715b-default-rtdb.firebaseio.com",
+  projectId: "data-3715b",
+  storageBucket: "data-3715b.firebasestorage.app",
+  messagingSenderId: "899957518828",
+  appId: "1:899957518828:web:4922e33655b4676244d0c9"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getDatabase(app);
+
+export { auth, db };
